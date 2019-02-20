76e3db4ff4acf9028207b0aeee2a19306c4a9b8f
Feras Allaou <feras@piyote.com>
2019-02-20 10:29:24 +0300

    added a git ignore file

---
+++ DragNDrop/BoardCollection+DragDrop.swift
@@ -0,0 +1,9 @@
+//
+//  BoardCollection+DragDrop.swift
+//  DragNDrop
+//
+//  Created by Feras Allaou on 2/19/19.
+//  Copyright © 2019 Feras Allaou. All rights reserved.
+//
+
+import Foundation
---
+++ DragNDrop/Models.swift
@@ -0,0 +1,9 @@
+//
+//  Models.swift
+//  DragNDrop
+//
+//  Created by Feras Allaou on 2/19/19.
+//  Copyright © 2019 Feras Allaou. All rights reserved.
+//
+
+import Foundation
